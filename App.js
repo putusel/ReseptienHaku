f71718092fbb2cb5685f99c941c116736168c86f
putusel <elina.putus@gmail.com>
2022-09-17 13:33:26 +0300

    API-haku toimii

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Dimensions } from 'react-native';
 
 export default function App() {
+
+const [keyword, setKeyword] = useState('');
+const [repositories, setRepositories] = useState([]);
+
+const getRepositories = () => {  
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)  
+  .then(response => response.json())  
+  .then(data => setRepositories(data.items))  
+  .catch(error => {         
+        Alert.alert('Error', error);   
+  });
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <FlatList
+        keyExtractor={(item,index) => index.toString()}  
+        renderItem={({item}) =>
+        <View>
+          <Text 
+            style={{fontSize:18, fontWeight: "bold"}}>{item.full_name}
+          </Text>
+          <Text style={{fontSize:16 }}>{item.description}</Text>
+        </View>} 
+      data={repositories} />
+      <TextInput 
+        style={{fontSize:18, width:200}} 
+        placeholder='keyword'onChangeText={text => setKeyword(text) } />
+      
+      <Button title="Find"onPress= {getRepositories} />
+  
+    <StatusBar style="auto" />
+  </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +46,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
 });
